b8daa11bd54fd1077936c9b774e7ccdef6af54fe
Farhaan Patel <patel.farhaaan@gmail.com>
2019-10-08 13:46:34 -0700

    muliple login established

--- project/users/views.py
+++ project/users/views.py
@@ -1,9 +1,10 @@
+import boto3
+import datetime
 from project import db
-from project.models import Users
-from project.users.forms import UpdateUserForm
-from project.users.picture_handler import add_profile_pic
+from project.models import Applicant, Resume
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, current_user, logout_user, login_required
-from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask import render_template, url_for, flash, redirect, request, Blueprint, session
 
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
@@ -13,13 +14,14 @@ users_blueprint = Blueprint('users', __name__, template_folder='templates')
 @login_required
 def logout():
     logout_user()
+    session.clear()
     flash('Logged Out')
     return redirect(url_for('users.login'))
 
 
 @users_blueprint.route('/register', methods=['GET','POST'])
 def register():
-
+    session.clear()
     if request.method == 'POST':
         email = request.form.get('email', None)
         name = request.form.get('name', None)
@@ -28,7 +30,7 @@ def register():
         repeat_password = request.form.get('repeat_password', None)
         dob = request.form.get('date_of_birth', None)
         resume = request.files.get('resume', None)
-        resume_name = resume.filename
+        resume_name = resume.filename.replace(' ', '')
 
         if email is None or email == '':
             return render_template('register.html', warning='Email cannot be Empty')
@@ -54,110 +56,65 @@ def register():
         if not (password == repeat_password):
             return render_template('register.html', warning='Both passwords should be same.')
 
+        user = Applicant.query.filter_by(email=email).first()
 
+        if user == None:
+            new_user_obj = Applicant(name=name,
+                                     email=email,
+                                     gender=gender,
+                                     birth_date=datetime.datetime.strptime(dob, '%m/%d/%Y'),
+                                     hashed_password=generate_password_hash(password))
+            db.session.add(new_user_obj)
+            db.session.commit()
 
-        import boto3
-
-        bucket = 'application-tracking-system-test'
-        s3 = boto3.client('s3',
-                          aws_access_key_id='ASIAXORRZPURJFTDV6NH',
-                          aws_secret_access_key='PKJW2YdtuWauC69t06R8cyoB9p1zW6vBXy0tu6ls',
-                          aws_session_token='FQoGZXIvYXdzEDoaDHCIiNOXHkcuipq8/CKCAkOCge4lCXFZIwNRkXmSXKniaZ8yDRKKDbw6LFv3k9eyaa21xolA5XA5L9U2T7ucZ+zHqGbvhM89yuDFV4RGGTCBYRtxZ7vE70pvegzVlXp7zWcR1TsnVmnSfyRuKMITOt3uq0+QG3uLOj4uQ6MWmaFRbumuwe78UFai/TTDsSQ6nv+JIY8+RYCpU3MZ+167p2qVfECNLJAhH01xc/hy/miHkbcwKoLT6AP6Rp3rbDsZvSneggh7W6lcs+72Ls/ ETgapyfsWhbKtF / lk1OPrQqhtWo6P0IKhyRabWOhaRvzpPZPrGSxVyTH1UwWylI5UpteF0upzU3afg5rhXW6l4tPa6yiL69bsBQ=='
-                          )
-
-        def upload_file_to_s3(file, bucket_name, acl="public-read"):
-
-            try:
-
-                resp = s3.upload_fileobj(
-                    file,
-                    bucket_name,
-                    file.filename,
-                    ExtraArgs={
-                        "ACL": acl,
-                        "ContentType": file.content_type
-                    }
-                )
-                print(resp)
-
-            except Exception as e:
-                # This is a catch all exception, edit this part to fit your needs.
-                print("Something Happened: ", e)
-
-        upload_file_to_s3(resume, bucket)
-
-
-
-        # existing_user_email = Users.query.filter_by(email=email).first()
-        # existing_user_username = Users.query.filter_by(username=username).first()
-
-        # if existing_user_email is not None:
-        #     return render_template('register.html', warning='Email already exists. Please login to continue')
-        # elif existing_user_username is  not None:
-        #     return render_template('register.html', warning='Username already exists. Please try with a new username')
-
-        # if password != repeat_password:
-        #     return render_template('register.html', warning='Passwords should be same!!!')
-        # else:
-            # new_user = User(email=email,
-                            # username=username.replace(' ', ''),
-                            # password=password)
-
-            # db.session.add(new_user)
-            # db.session.commit()
-            # return redirect(url_for('users.login'))
+            user = Applicant.query.filter_by(email=email).first()
+            public_resume_link = file_upload_to_s3(resume, resume_name)
+            new_resume_obj = Resume(app_id=user.id,
+                                    resume=public_resume_link)
+            db.session.add(new_resume_obj)
+            db.session.commit()
+            return redirect(url_for('users.login'))
+        else:
+            return render_template('register.html', warning='Email already exists. Please Login')
     return render_template('register.html')
 
 
 @users_blueprint.route('/login', methods=['GET','POST'])
 def login():
-
+    session.clear()
     if request.method == 'POST':
         email = request.form.get('email', 'None')
         password = request.form.get('password', 'None')
-        user = User.query.filter_by(email=email).first()
+        user = Applicant.query.filter_by(email=email).first()
 
         if user == None:
             return render_template('login.html', warning='Email Does not exist!!!')
-        elif user.check_hashed_password(password) and user is not None:
+        elif check_password_hash(user.hashed_password, password) and user is not None:
             login_user(user)
-            return redirect(url_for('core.index'))
+            session['user_email'] = user.email
+            print("user is logged in!!!", current_user.is_authenticated)
+            return redirect(url_for('users.applicant_view'))
         else:
             return render_template('login.html', warning='Password is incorrect')
     return render_template('login.html')
 
 
-@users_blueprint.route('/account', methods=['GET','POST'])
+@users_blueprint.route('/applicant-view', methods=['GET', 'POST'])
 @login_required
-def account():
-    form = UpdateUserForm()
-
-    if form.validate_on_submit():
-
-        if form.picture.data:
-            username = current_user.username
-            pic = add_profile_pic(form.picture.data,username)
-            current_user.profile_image = pic
-
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Information updated Successfully')
-        return redirect(url_for('users.account'))
-
-    elif request.method == 'GET':
-        form.email.data = current_user.email
-        form.username.data = current_user.username
-
-    profile_image = url_for('static', filename='profile_pics/'+current_user.profile_image)
-    return render_template('account.html', profile_image=profile_image, form=form)
+def applicant_view():
+    return render_template('applicant-view.html')
 
 
-# @users_blueprint.route('/<username>')
-# def user_posts(username):
-#     page = request.args.get('page', 1, type=int)
-#     user = Users.query.filter_by(username=username).first_or_404()
-    # blog_post = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page,per_page=5)
-    # return render_template('user_blog_post.html',
-    #                         blog_post=blog_post,
-    #                         user=user)
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+def file_upload_to_s3(file, object_name):
+    bucket = 'putbox-darshan'
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id='AKIA5SX2735H2JOS3RN2',
+        aws_secret_access_key='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'
+    )
+    s3.upload_fileobj(file, bucket, object_name, ExtraArgs={"ACL": "public-read"})
+    public_url = f"https://putbox-darshan.s3-us-west-1.amazonaws.com/{object_name}"
+    return public_url
